d14feebae501b42a4e3a2e28285d1ee7008129fa
Temuulen Sengedorj <tsengedorj21@my.whitworth.edu>
2019-03-20 21:03:56 -0700

    Added manual entry

--- MixingPot/MixingPot/Form3.cs
+++ MixingPot/MixingPot/Form3.cs
@@ -21,5 +21,42 @@ namespace MixingPot
 		{
 
 		}
-	}
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+            textBox7.Clear();
+            textBox8.Clear();
+            textBox9.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Hide();
+            Form4 f4 = new Form4();
+            f4.ShowDialog();
+            Close();
+
+        }
+    }
 }
