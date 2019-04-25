58db2b9052f159b5e0a0690fc3193d29036dc2ef
Temuulen Sengedorj <tsengedorj21@my.whitworth.edu>
2019-04-24 19:39:47 -0700

    Edited user interface

--- MixingPot/MixingPot/Form4.Designer.cs
+++ MixingPot/MixingPot/Form4.Designer.cs
@@ -28,71 +28,78 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.label1 = new System.Windows.Forms.Label();
-			this.button3 = new System.Windows.Forms.Button();
-			this.userControl11 = new MixingPot.UserControl1();
-			this.button2 = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// label1
-			// 
-			this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
-			this.label1.AutoSize = true;
-			this.label1.Font = new System.Drawing.Font("Calibri", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(547, 46);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(401, 78);
-			this.label1.TabIndex = 6;
-			this.label1.Text = "Assign Groups";
-			// 
-			// button3
-			// 
-			this.button3.Anchor = System.Windows.Forms.AnchorStyles.None;
-			this.button3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-			this.button3.Location = new System.Drawing.Point(1196, 308);
-			this.button3.Name = "button3";
-			this.button3.Size = new System.Drawing.Size(119, 32);
-			this.button3.TabIndex = 8;
-			this.button3.Text = "Add another location";
-			this.button3.UseVisualStyleBackColor = false;
-			this.button3.Click += new System.EventHandler(this.button3_Click);
-			// 
-			// userControl11
-			// 
-			this.userControl11.Anchor = System.Windows.Forms.AnchorStyles.None;
-			this.userControl11.Location = new System.Drawing.Point(97, 175);
-			this.userControl11.Name = "userControl11";
-			this.userControl11.Size = new System.Drawing.Size(1300, 127);
-			this.userControl11.TabIndex = 10;
-			// 
-			// button2
-			// 
-			this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
-			this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
-			this.button2.Location = new System.Drawing.Point(1203, 80);
-			this.button2.Name = "button2";
-			this.button2.Size = new System.Drawing.Size(112, 45);
-			this.button2.TabIndex = 11;
-			this.button2.Text = "Finish and Display Groups";
-			this.button2.UseVisualStyleBackColor = false;
-			this.button2.Click += new System.EventHandler(this.button2_Click);
-			// 
-			// Form4
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.AutoScroll = true;
-			this.ClientSize = new System.Drawing.Size(1478, 830);
-			this.Controls.Add(this.button2);
-			this.Controls.Add(this.userControl11);
-			this.Controls.Add(this.button3);
-			this.Controls.Add(this.label1);
-			this.Name = "Form4";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "MixingPot - Select Groups";
-			this.Load += new System.EventHandler(this.Form4_Load);
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.userControl11 = new MixingPot.UserControl1();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(352, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(709, 112);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Assign Groups";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button3.BackColor = System.Drawing.Color.Coral;
+            this.button3.Location = new System.Drawing.Point(1122, 287);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 43);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Add another location";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.BackColor = System.Drawing.Color.Coral;
+            this.button2.Location = new System.Drawing.Point(1253, 287);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 45);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Finish and Display Groups";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // userControl11
+            // 
+            this.userControl11.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.userControl11.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userControl11.Location = new System.Drawing.Point(12, 140);
+            this.userControl11.Name = "userControl11";
+            this.userControl11.Size = new System.Drawing.Size(1300, 127);
+            this.userControl11.TabIndex = 10;
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackgroundImage = global::MixingPot.Properties.Resources.whitworth_logo_horizontal_reverse_rgb;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1478, 830);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.userControl11);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form4";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MixingPot - Select Groups";
+            this.Load += new System.EventHandler(this.Form4_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
